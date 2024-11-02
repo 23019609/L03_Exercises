fced1332aaea8860d8fabbb3642fa66d536b1651
23019609 <23019609@myrp.edu.sg>
2024-11-03 00:02:14 +0800

    lesson 3 exercises

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StatusBar} from 'expo-status-bar';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Button,
+    Alert,
+    TouchableOpacity,
+    ToastAndroid,
+    ScrollView
+} from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const MyApp = () => {
+    const [username, setUsername] = useState('');
+    const [usertype, setUsertype] = useState('');
+
+    return (
+        <View style={{padding: 30, paddingTop: 50, paddingBottom: 50}}>
+            <ScrollView style={{marginBottom: 30}}>
+
+                {/*EXERCISE 1B*/}
+                <Text>User Type:</Text>
+                <RNPickerSelect
+                    onValueChange={(value) => {
+                        setUsertype(value)
+                    }}
+                    items={[
+                        {label: 'Admin', value: 'Admin'},
+                        {label: 'Guest', value: 'Guest'},
+                    ]}
+                />
+
+                {/*EXERCISE 1G*/}
+                <Text>User Name:</Text>
+                <TextInput
+                    style={{borderWidth: 1}}
+                    onChangeText={(text) => {
+                        setUsername(text)
+                    }}
+                />
+
+                {/*EXERCISE 1A*/}
+                <Text>Password: </Text>
+                <TextInput style={{borderWidth: 1}}/>
+
+                {/*EXERCISE 1C & 1D*/}
+                {/*<Button*/}
+                {/*    title="LOG IN"*/}
+                {/*    onPress={() => Alert.alert("Welcome!")}*/}
+                {/*/>*/}
+
+                {/*EXERCISE 1E*/}
+                {/*<TouchableOpacity onPress={() => Alert.alert("Welcome!")}>*/}
+                {/*  <Text>LOG IN</Text>*/}
+                {/*</TouchableOpacity>*/}
+
+                {/*EXERCISE 1F & 1G*/}
+                <TouchableOpacity
+                    onPress={() => ToastAndroid.show(`Welcome ${usertype} ${username}!`, ToastAndroid.SHORT)}>
+                    <Text>LOG IN</Text>
+                </TouchableOpacity>
+
+            </ScrollView>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+
+export default MyApp;
